bccc92f751112b64f020b0197993f2f9e040744c
Rob-Yoo <jin12243@naver.com>
2022-07-12 21:58:36 +0900

    Feat : parsing

---
+++ src/parsing/parse_color.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_color.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 20:48:27 by jinyoo            #+#    #+#             */
+/*   Updated: 2022/07/12 20:49:59 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+char	*access_information(char *line)
+{
+	while (is_upper(*line) == TRUE)
+		line++;
+	while (is_space(*line) == TRUE)
+		line++;
+	return (line);
+}
+
+static int	parse_color_handler(char *line)
+{
+	int	i;
+	int	r;
+	int	g;
+	int	b;
+	int	color;
+
+	i = -1;
+	while (line[++i] && ft_isdigit(line[i]))
+		r = r * 10 + (line[i] - '0');
+	while (line[++i] && ft_isdigit(line[i]))
+		g = g * 10 + (line[i] - '0');
+	while (line[++i] && ft_isdigit(line[i]))
+		b = b * 10 + (line[i] - '0');
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+		exit_error("Wrong RGB range");
+	color = (r * 256 * 256) + (g * 256) + b;
+	return (color);
+}
+
+int	parse_color(char *line)
+{
+	int		color;
+	char	**color_malloc;
+
+	line = access_information(line);
+	color = parse_color_handler(line);
+	return (color);
+}
