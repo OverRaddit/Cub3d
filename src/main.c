b6d93f00b465324c7bb4a33ace6a50f83681701f
Rob-Yoo <jin12243@naver.com>
2022-07-12 21:58:10 +0900

    Fix : main.c

--- src/main.c
+++ src/main.c
@@ -6,16 +6,22 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 15:50:46 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/07/11 21:35:29 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/07/12 16:47:09 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../include/cub3d.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	printf("hi\n");
+	t_map	map;
+
+	if (argc != 2)
+		ft_putendl_fd("Error", 2);
+	else
+	{
+		ft_memset(&map, 0, sizeof(t_map));
+		parse(&map, argv[1]);
+	}
 	return 0;
 }
